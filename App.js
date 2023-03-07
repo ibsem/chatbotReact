f269438ec3bc20d679e3522a3c45b0fd8228c1e8
ibsemdias <ibsem.dias@gmail.com>
2023-03-07 19:39:27 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, TextInput, Button } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const chatbotApi = axios.create({
+  baseURL: 'http://127.0.0.1:8000', // URL da API Python FastAPI
+  timeout: 1000,
 });
+
+const Chatbot = () => {
+  const [inputText, setInputText] = useState('');
+  const [responseText, setResponseText] = useState('');
+
+  const handleInputTextChange = (text) => {
+    setInputText(text);
+  };
+
+  const handleButtonClick = async () => {
+    try {
+      const response = await chatbotApi.post('/chatbot', {
+        input_text: inputText,
+      });
+      setResponseText(response.data.response_text);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Chatbot</Text>
+      </View>
+      <View>
+        <TextInput
+          placeholder="Digite sua mensagem"
+          value={inputText}
+          onChangeText={handleInputTextChange}
+        />
+        <Button title="Enviar" onPress={handleButtonClick} />
+      </View>
+      <View>
+        <Text>{responseText}</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Chatbot;
